ea1d329ce83d615648d253be34e41cc79d3342bf
SmailyBarath <115382856+SmailyBarath@users.noreply.github.com>
2022-12-02 20:08:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+USERNAME = "JokerProtectBot"
+BOT_ID = 5851861328
+COMMANDS = [".","?","/","!","$","#","~",]
+GROUP_ID = -1001572192111
+LOG_CHANNEL_ID = -1001572192111
+GROUP_URL = "t.me/JackSparrowSupport"
+CHANNEL_ID = -1001572192111
+CHANNEL_URL = "t.me/JackSparrowUpdates"
