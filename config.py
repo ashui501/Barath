e66a06c88ca5e35293a0fe992301524367a1dddd
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-23 07:41:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
-USERNAME = "JokerProtectBot"
-BOT_ID = 5851861328
-COMMANDS = [".","?","/","!","$","#","~",]
-GROUP_ID = -1001572192111
-LOG_CHANNEL_ID = -1001572192111
-GROUP_URL = "t.me/JackSparrowSupport"
-CHANNEL_ID = -1001572192111
-CHANNEL_URL = "t.me/JackSparrowUpdates"
+LOG_GROUP_ID = -1001785150977
+OWNER_ID = [1666544436]
+HANDLER = ["~", ".","!","?","@","$"]
+ALIVE_TEXT = """ **~  ʙᴀʀᴀᴛʜ  sʏsᴛᴇᴍ:**
+━━━━━━━━━━━━━━━━━━━
+~ **ᴏᴡɴᴇʀ**: {}
+~ **ᴠᴇʀsɪᴏɴ:** {}
+~ **ᴘᴏɴɢ ᴍs:** {}
+~ **ᴜᴘᴛɪᴍᴇ:** {}
+━━━━━━━━━━━━━━━━━━━
+"""
+
+BARATH = "https://telegra.ph/file/3ce4da02ae84dd2097824.jpg"
