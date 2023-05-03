5aede50e6f39243ee6f4d88f5b4a22b49b2d80e8
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-05-03 09:55:32 +0530

    Update rename.py

--- Barath/plugins/rename.py
+++ Barath/plugins/rename.py
@@ -31,7 +31,7 @@ async def rename(_, message):
         filename = "{name}.{filetype}".format(name=name, filetype=filetype)
     msg = await message.reply_text("⬇️ File has downloading...")
     path = await message.reply_to_message.download(file_name=filename)
-    thumb_id = ".Barath/barath_img/IMG_20230503_093609_915.jpg"
+    thumb_id = "Barath/barath_img/IMG_20230503_093609_915.jpg"
     await msg.edit_text("⬆️ File has uplaoding")
     await message.reply_document(document=path, thumb=thumb_id)
     await msg.delete()
